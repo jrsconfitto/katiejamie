21ba016f8c2921e189243087d758fdd0d20e38bc
James Sconfitto <james@exele.com>
2012-12-21 14:00:24 -0500

    ease into it
    Yeah, it's terrible code. That's fine, just need it to work for a little
    bit ;)

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ $(function(){
 
 var katieAndJamieSpreadsheet = 'https://docs.google.com/spreadsheet/pub?key=0AmhWglGO45rldGNoYy12ZDk2bU1EWTRZTWFtVEpJd2c&output=html';
 var happenings;
+var theMap;
+var theMarkerLayer;
 
 // Read the spreadsheet first, then execute the callback
 function init() {
@@ -19,14 +21,16 @@ function showInfo(data, tabletop) {
 
   // Create a map... with yet another callback
   mapbox.auto('map', 'jugglingnutcase.map-rxoo73qt', function(map) {
+    theMap = map;
+
     // Create an empty markers layer
-    var markerLayer = mapbox.markers.layer();
+    theMarkerLayer = mapbox.markers.layer();
 
     // Add interaction to this marker layer. This
     // binds tooltips to each marker that has title
     // and description defined.
-    mapbox.markers.interaction(markerLayer);
-    map.addLayer(markerLayer);
+    mapbox.markers.interaction(theMarkerLayer);
+    theMap.addLayer(theMarkerLayer);
 
     // Convert the data from the spreadsheet into features for insertion as markers
     happenings = data.map(function(happyHappening) {
@@ -52,7 +56,27 @@ function showInfo(data, tabletop) {
     console.log(happenings);
 
     // Add the markers into the map!
-    markerLayer.features(happenings);
+    theMarkerLayer.features(happenings);
+
+    // Set an interval to ease between markers
+    var currentMarkerIndex = 0;
+    var totalMarkers = happenings.length;
+
+    var markers = theMarkerLayer.markers()
+    setInterval(function() {
+      // Ease to the marker
+      console.log('Take it easey bro');
+      var location = markers[currentMarkerIndex].location;
+      theMap.ease.location(location).zoom(15).optimal();
+
+      // Increment the marker or start over again
+      if  (currentMarkerIndex >= totalMarkers) {
+        currentMarkerIndex = 0; 
+      }
+      else {
+        currentMarkerIndex++;
+      }
+    }, 10000);
   });
 }
 
